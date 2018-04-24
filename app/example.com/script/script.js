b017c48000f701b893b7822e4609bb14c17431a1
Filip Hanberg <9902fiha@res.ludvika.intra>
2018-04-24 10:57:18 +0200

    closes #3 and closes #2

--- app/example.com/script/script.js
+++ app/example.com/script/script.js
@@ -3,7 +3,7 @@ elements.forEach( element => {
     element.addEventListener("click", (event) =>{ //ett inlägg tas bort när dess "delete"-knapp trycks
         const posid = element.dataset.id; //hämtar inläggets id genom attributet "data-id" i Admin Dashboard
         event.preventDefault(); //stoppar länken från att skicka användaren till en annan sida
-        fetch("delete.php?id=" + posid) //läser in php-filen och tar bort inlägget
+        fetch("delete.php?id=" + posid) //skickar ett HTTP-GET-Request och tar bort inlägget
         .then(function(){
             const post= document.getElementById("post-"+posid);
             post.innerHTML = ""; //gömmer inlägget som just togs bort
